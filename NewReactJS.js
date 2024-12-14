22f731a23046342f8ed13e51d6b64ccc5601d3c2
Khushabo07 <khushabookumawat07@gmail.com>
2024-12-14 15:35:28 +0530

    First

---
+++ NewReactJS.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", {id:"heading"} , "Hello World from React!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const Newheading = React.createElement("h1",{},"Hello Sona!!! How are you??hi i am ok how are you i study in class 5th you no that.");
+const NewRoot = ReactDOM.createRoot(document.getElementById("NewRoot"));
+NewRoot.render(Newheading);
+
+
+
